2dd4c25e915d7a4160ed90ddd1138206f919d0ea
Selçuk Özer <selcuk.ozer.y7657@student.hogent.be>
2023-06-08 17:15:03 +0200

    angular

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppDriversComponent } from './app/app-drivers/app-drivers.component';
 import { AppHomeComponent } from './app/app-home/app-home.component';
 import { ErrorComponent } from './app/error/error.component';
 import { AppComponent } from './app/app.component';
-import { AppDriverDetailComponent } from './app/app-driver-detail copy/driverDetail.component';
+import { AppDriverDetailComponent } from './app/app-driver-detail/driverDetail.component';
 import { AppDriverEditComponent } from './app/app-driver-edit/driverEdit.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppVehiclesComponent } from './app/app-vehicles/app-vehicles.component';
